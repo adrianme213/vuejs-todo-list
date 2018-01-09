5f3894719681481d30e4514d7a3b736d8a003c3d
Adrian Meza <adrianme213@gmail.com>
2018-01-09 09:26:33 -0800

    [feat] - delete todo button works

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -11,6 +11,9 @@
         <span class='right floated edit icon' v-on:click="showForm">
           <i class='edit icon'></i>
         </span>
+        <span class='right floated trash icon' v-on:click="deleteTodo(todo)">
+          <i class='trash icon'></i>
+        </span>
       </div>
     </div>
     <!-- form is visible when we are in editing mode -->
@@ -57,6 +60,9 @@ export default {
     hideForm() {
       this.isEditing = false;
     },
+    deleteTodo(todo) {
+      this.$emit('delete-todo', todo);
+    }
   },
 };
 </script>
